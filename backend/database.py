053d939a698cbde0cdb4f12d39dbb3105d9d187d
AyushRaj8271 <analyst.ayush@gmail.com>
2025-09-07 15:00:04 +0530

    database connection on render

--- backend/database.py
+++ backend/database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5440/taskdb"
+DATABASE_URL_PROD_INTERNAL ="postgresql://taskdb_hh6i_user:k6yU4yagatIbQPaBCS7mPZds5fWs4j5J@dpg-d2ukt9nfte5s73b8vc00-a/taskdb_hh6i"
 
-
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL_PROD_INTERNAL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
